bcf4c93c4a3901a24f8ba82c98a2d81318680f02
chenjiecqupt <904281072@qq.com>
2017-06-29 09:08:46 +0800

    test
    测试ejs模板导入功能

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var json1 = { title: 'Express' ,str: 'info for users'};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    var method = req.method.toUpperCase();
+    var proxy_url = 'test.json';
+
+    var options = {
+        headers: {"Connection": "close"},
+        url: proxy_url,
+        method: method,
+        json: true,
+        body: req.body
+    };
+
+    function callback(error, response, data) {
+        if (!error && response.statusCode == 200) {
+            console.log('------接口数据------',data);
+
+            res.json(data)
+        }
+    }
+
+    request(options, callback);
     res.render('test1', json1);
     /*res.send('respond with a resource');*/
 });
@@ -11,3 +32,32 @@ router.get('/a', function(req, res, next) {
     /*res.send('respond with a resource');*/
 });
 module.exports = router;
+/*
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.all('/!*', function(req, res){
+    var method = req.method.toUpperCase();
+    var proxy_url = 'http:www.xx.com/api';
+
+    var options = {
+        headers: {"Connection": "close"},
+        url: proxy_url,
+        method: method,
+        json: true,
+        body: req.body
+    };
+
+    function callback(error, response, data) {
+        if (!error && response.statusCode == 200) {
+            console.log('------接口数据------',data);
+
+            res.json(data)
+        }
+    }
+
+    request(options, callback);
+})
+
+module.exports = router;*/
